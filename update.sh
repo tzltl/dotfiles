ae6740550bede5507f3f9330d7f563481c12d50c
tzltl <takeshi.x.6321@gmail.com>
2021-07-21 20:36:38 +0900

    alacrittyのconfig追加

--- update.sh
+++ update.sh
@@ -6,3 +6,4 @@ cp ~/.tmux-powerlinerc ~/dotfiles/.tmux/
 cp -r ~/tmux-powerline/themes/2020tmuxpowerlinetheme.sh ~/dotfiles/.tmux/tmux-powerline/themes/
 cp -r ~/.config/nvim ~/dotfiles/.config/
 cp ~/.oh-my-zsh/themes/2020theme.zsh-theme ~/dotfiles/.oh-my-zsh/themes/
+cp -r ~/.config/alacritty ~/dotfiles/.config/alacritty
